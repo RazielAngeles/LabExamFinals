eef7c356bf65c76cb282fca23bc0802bbf86dbb6
Anenias <aneniasrussel38@gmail.com>
2025-05-11 15:34:16 +0800

    Update urls.py

--- library-management-system/library_backend/library/urls.py
+++ library-management-system/library_backend/library/urls.py
@@ -1,1 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, borrow_book, return_book, all_transactions
 
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('api/borrow/', borrow_book),
+    path('api/return/<int:borrow_id>/', return_book),
+    path('api/transactions/', all_transactions),
+]
