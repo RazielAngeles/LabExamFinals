56976dac5d7f68d8c8c809f56c3add3438f7c4ab
Anenias <aneniasrussel38@gmail.com>
2025-05-11 15:30:44 +0800

    Update serializers.py

--- library-management-system/library_backend/library/serializers.py
+++ library-management-system/library_backend/library/serializers.py
@@ -1,1 +1,18 @@
+from rest_framework import serializers
+from .models import Book, BorrowTransaction
+from django.contrib.auth.models import User
 
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+class BorrowTransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BorrowTransaction
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
