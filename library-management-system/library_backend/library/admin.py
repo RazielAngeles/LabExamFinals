c00ef941b5cfcebfbe1de09656cc6f3faf104f5b
Anenias <aneniasrussel38@gmail.com>
2025-05-11 18:11:17 +0800

    Update admin.py

--- library-management-system/library_backend/library/admin.py
+++ library-management-system/library_backend/library/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book, BorrowTransaction
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn', 'copies_available')
+    search_fields = ('title', 'author', 'isbn')
+
+@admin.register(BorrowTransaction)
+class BorrowTransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'status', 'borrow_date', 'return_date')
+    list_filter = ('status', 'borrow_date')
+    search_fields = ('user__username', 'book__title')
